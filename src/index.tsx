e6906ecb52b6308aa3f74b6c8de1a379af3534cb
skytomo <tomo1kawa.fullerene@gmail.com>
2024-03-27 20:56:53 +0900

    feat: git init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App";
+
+const container = document.getElementById("root");
+if (container === null)
+  throw new Error("No root element found in the document");
+const root = createRoot(container);
+root.render(<App />);
