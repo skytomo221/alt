e4c92f8201fb912e7656eda7ab0a54a80e323d0c
skytomo <tomo1kawa.fullerene@gmail.com>
2024-03-28 01:10:26 +0900

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import ai from "./assets/images/ai.png";
+
 export function App(): JSX.Element {
-  return <div>Hello, React!</div>;
+  return (
+    <main>
+      <h1>ALt</h1>
+      <div className="text-3xl font-bold underline">Hello, React!</div>
+      <img src={ai} />
+    </main>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,49 +11,53 @@ const stylesHandler = 'style-loader';
 
 
 const config = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.png$/,
+        use: ["url-loader"],
+      },
+      // {
+      //     test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+      //     type: 'asset',
+      // },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
 };
 
 module.exports = () => {
